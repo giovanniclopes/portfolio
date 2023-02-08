3a976041d0e4d89d6a9f5b6aa80c8b97f0db6018
Giovanni Lopes <giovanniclopes@gmail.com>
2023-02-07 23:20:40 -0300

    att Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { Link, NavLink } from "react-router-dom";
 import { useState } from "react";
-import { List, X } from "phosphor-react";
+import { List, X, LinkedinLogo, GithubLogo , EnvelopeSimple } from "phosphor-react";
 
 export function Header() {
   const [togglerNav, setTogglerNav] = useState(false);
@@ -30,8 +30,8 @@ export function Header() {
       <nav
         className={
           fix
-            ? "z-50 fixed top-0 w-full flex items-center justify-between h-max py-7 px-8 mx-auto bg-gray-500 text-white font-semibold transition-all md:items-center mbl:px-4"
-            : "z-50 fixed top-0 w-full flex items-center justify-between h-24 py-3 px-8 mx-auto backdrop-blur-0 bg-gray-600/20 font-medium text-white/50 transition-all md:items-center mbl:px-4"
+            ? "z-50 fixed top-0 w-full flex items-center justify-between h-max py-7 px-8 mx-auto bg-gray-500 text-white text-sm font-semibold transition-all md:items-center mbl:px-4"
+            : "z-50 fixed top-0 w-full flex items-center justify-between h-24 py-3 px-8 mx-auto backdrop-blur-0 bg-gray-600/20 text-xs font-medium text-white/50 transition-all md:items-center mbl:px-4"
         }
       >
         {/* <Link to="/">
@@ -46,36 +46,47 @@ export function Header() {
           className={
             togglerNav
               ? "absolute left-0 top-0 w-screen mt-20 border-t border-red-500 p-6 flex items-left flex-col gap-4 text-gray-300 font-nordica text-2xl bg-gray-600 md:flex md:items-center"
-              : "hidden gap-4 text-lg md:flex md:items-center"
+              : "hidden gap-4 md:flex md:items-center"
           }
         >
           <NavLink
-            className="transition-all hover:text-white"
+            className="transition-all hover:text-gray-50"
             onClick={clickHandler}
             to="#about-me"
           >
             Sobre mim
           </NavLink>
           <NavLink
-            className="transition-all hover:text-white"
+            className="transition-all hover:text-gray-50"
             onClick={clickHandler}
             to="#skills"
           >
             Skills
           </NavLink>
           <NavLink
-            className="transition-all hover:text-white"
+            className="transition-all hover:text-gray-50"
             onClick={clickHandler}
             to="#projects"
           >
             Projetos
           </NavLink>
           <NavLink
-            className="transition-all hover:text-white"
+            className="transition-all hover:text-gray-50"
             onClick={clickHandler}
             to="#contact"
           >
             Contato
+          </NavLink>
+        </div>
+        <div className="flex gap-5">
+          <NavLink to="https://linkedin.com/in/giovanni-lopes21">
+            <LinkedinLogo width={26} height={26} />
+          </NavLink>
+          <NavLink to="https://github.com/giovanniclopes">
+            <GithubLogo width={26} height={26} />
+          </NavLink>
+          <NavLink to="emailto:giovanniclopes@gmail.com">
+            <EnvelopeSimple width={26} height={26} />
           </NavLink>
         </div>
         <button
