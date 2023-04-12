f34401b6c57d2213959b71f0becc3b127ec4b3bc
Giovanni Lopes <giovanniclopes@gmail.com>
2023-04-11 21:40:29 -0300

    "vibrance" project added

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,10 +4,9 @@ import SectionTitles from "./SectionTitles";
 
 import DangerNet from "../assets/projects/project-dangernet.webp";
 import EventPlatform from "../assets/projects/project-eventplatform.webp";
-import Calculator from "../assets/projects/project-calculator.webp";
 import Nordician2 from "../assets/projects/project-nordician2.webp";
 import EdgeLine from "../assets/projects/project-edgeline.webp";
-import ArrowDrawIcon from "../assets/icons/ArrowDrawIcon";
+import Vibrance from "../assets/projects/project-vibrance.webp";
 
 export default function Projects() {
   return (
@@ -32,20 +31,30 @@ export default function Projects() {
             "Olá viajante, bem vinda(o) ao site Nordician. Aqui forneço diversos artigos para quem busca por informações sobre a cultura nórdica. Contendo artigos sobre o mundo real, e sobre o mundo místico."
           }
         />
-        <ProjectBlock
-          linkURL={"https://giovanniclopes.github.io/EdgeLine/"}
-          imgURL={EdgeLine}
-          title={"EdgeLine"}
-          description={"Protótipo de website de vendas de HQs."}
-        />
+          <ProjectBlock
+            linkURL={
+              "https://www.figma.com/proto/UFZMwTJIpG2yoSA7GgTFAD/Vibrance?page-id=0%3A1&node-id=1-3&viewport=819%2C316%2C0.15&scaling=min-zoom&starting-point-node-id=351%3A599"
+            }
+            imgURL={Vibrance}
+            title={"Vibrance"}
+            description={
+              "Vibrance é um site fictício de vendas online, onde é possível comprar livros físicos e digitais."
+            }
+          />
         <ProjectBlockReverse
-          linkURL={"https://giovanniclopes.github.com/dangernet"}
+          linkURL={"https://giovanniclopes.github.io/dangernet/"}
           imgURL={DangerNet}
           title={"Danger Net"}
           description={
             "Aqui você irá descobrir os mistérios e as verdades pro trás da grande internet."
           }
         />
+          <ProjectBlock
+            linkURL={"https://giovanniclopes.github.io/EdgeLine/"}
+            imgURL={EdgeLine}
+            title={"EdgeLine"}
+            description={"Protótipo de website de vendas de HQs."}
+          />
       </div>
 
       <div className="flex flex-row items-center justify-end gap-4 mt-7">
